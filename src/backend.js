7b02bb38e87fb84b282877a9f453217cd2f9d6d6
DEH <156702447+DEH10@users.noreply.github.com>
2024-06-13 18:29:26 +0000

    commit message

--- src/backend.js
+++ src/backend.js
@@ -2,6 +2,8 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({ origin: true });
 const axios = require('axios');
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage();
 
 admin.initializeApp();
 
@@ -28,6 +30,16 @@ exports.submitSurvey = functions.https.onCall(async (data, context) => {
     // Save data to Firestore
     const surveysCollection = firestore.collection('Surveys');
     await surveysCollection.add(data);
+
+    // Save data to Cloud Storage (react-survey-backend.appspot.com/Surveys)
+    const bucket1 = storage.bucket('react-survey-backend.appspot.com');
+    const file1 = bucket1.file('Surveys/' + Date.now() + '.json');
+    await file1.save(JSON.stringify(data));
+
+    // Save data to Cloud Storage (prj_od4Cwh377TlTMBRdABwq6pGKYSgR/r0R13LIrxJURMgB4dlNS)
+    const bucket2 = storage.bucket('prj_od4Cwh377TlTMBRdABwq6pGKYSgR');
+    const file2 = bucket2.file('r0R13LIrxJURMgB4dlNS/' + Date.now() + '.json');
+    await file2.save(JSON.stringify(data));
 
     return { message: 'Survey submitted successfully' };
   } catch (error) {
